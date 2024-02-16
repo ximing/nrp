66f4d5b2910cb03b491e13855cd30c7723961a48
ximing <ximing@meituan.com>
2024-02-16 14:39:43 +0800

    feat: add dashboard support

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -10,6 +10,7 @@ export interface ClientSettings {
   subdomain_host?: string;
   timeout?: number;
   retry?: number;
+  log_file?: string;
   backoff?: {
     min: number;
     max: number;
@@ -22,4 +23,12 @@ export interface NrpServerConfig {
   port: number;
   vhost_http_port: number;
   vhost_https_port?: number;
+  log_file?: string;
+}
+
+export interface NrpDashboardConfig {
+  dashboard_addr: string;
+  dashboard_port: number;
+  dashboard_user: string;
+  dashboard_pwd: string;
 }
